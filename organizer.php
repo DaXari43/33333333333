d9bc700851d8c6816e386ebc157a2a88b12c22f2
DaXari <weidor4343@gmail.com>
2025-03-05 13:35:00 +0100

    Basic html

---
+++ organizer.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Organizer</title>
+    <link rel="stylesheet" href="styl6.css">
+</head>
+<body>
+    <header>
+        <div>
+            <h2>MÓJ ORGANIZER</h2>
+        </div>
+
+        <div>
+            <form action="organizer.php" method="post">
+                Wpis wydarzenia: <input type="text" name="wydarzenie" id="">
+                <button type="submit">ZAPISZ</button>
+            </form>
+        </div>
+
+        <div>
+            <img src="logo2.png" alt="Mój organizer">
+        </div>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+        <p>Stronę wykonał: 00000000000</p>
+    </footer>
+</body>
+</html>
